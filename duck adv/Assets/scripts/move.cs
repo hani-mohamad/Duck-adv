401472d164405fe874c1d97f01be25eccf2e0983
Hani <hanimohamedhm911@gmail.com>
2022-05-26 00:05:39 +0100

    asfasfsaf

---
+++ duck adv/Assets/scripts/move.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move: MonoBehaviour
+{
+
+    public float movespeed = 4f; // how fast
+
+    public Rigidbody2D rb;  // shortcut for rigidbody
+
+    Vector2 Movement;     // storing the x and y moevment in a vector2
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement.x = Input.GetAxisRaw("Horizontal"); // storing the x in the vector2
+        Movement.y = Input.GetAxisRaw("Vertical"); // storing the y in the vector2
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + Movement * movespeed * Time.fixedDeltaTime);
+
+    }
+
+}
+
